7719c29921ca05ffb3d29ba8408233439aa8d109
alireza <alireza.lm69@gmail.com>
2023-07-03 22:06:53 -0300

    new site

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
+<script>
+import feather from 'feather-icons';
+import AppHeader from './components/shared/AppHeader';
+import AppFooter from './components/shared/AppFooter';
+
+export default {
+	components: {
+		AppHeader,
+		AppFooter,
+	},
+	data: () => {
+		return {
+			appTheme: localStorage.getItem('theme'),
+		};
+	},
+	mounted() {
+		feather.replace();
+	},
+	updated() {
+		feather.replace();
+	},
+};
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div :class="appTheme" class="pt-0.5">
+		<!-- App header -->
+		<AppHeader />
+
+		<!-- Render active component contents with vue transition -->
+		<transition name="fade" mode="out-in">
+			<router-view :theme="appTheme" />
+		</transition>
+
+		<!-- Scroll to top -->
+		<back-to-top
+			visibleoffset="500"
+			right="30px"
+			bottom="20px"
+			class="shadow-lg"
+		>
+			<i data-feather="chevron-up"></i>
+		</back-to-top>
+
+		<!-- App footer -->
+		<AppFooter />
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
 }
 
-nav {
-  padding: 30px;
+.vue-back-to-top {
+	@apply p-2 bg-indigo-500 hover:bg-indigo-600 text-white transition
+        duration-500
+        ease-in-out
+        transform
+        hover:-translate-y-1 hover:scale-110;
+	border-radius: 50%;
+	font-size: 22px;
+	line-height: 22px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.fade-enter-active {
+	animation: coming 0.4s;
+	animation-delay: 0.2s;
+	opacity: 0;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.fade-leave-active {
+	animation: going 0.4s;
+}
+
+@keyframes going {
+	from {
+		transform: translateX(0);
+	}
+
+	to {
+		transform: translateX(-10px);
+		opacity: 0;
+	}
+}
+
+@keyframes coming {
+	from {
+		transform: translateX(-10px);
+		opacity: 0;
+	}
+
+	to {
+		transform: translateX(0px);
+		opacity: 1;
+	}
 }
 </style>
